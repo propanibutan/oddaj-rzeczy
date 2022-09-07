83dc9c5e8ea4f3519551393de6706ca6527e8a1f
Patrycja Zalas <minekosakura@gmail.com>
2022-09-07 14:32:25 +0200

    header background

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -1,57 +1,54 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Link as Scroll } from 'react-scroll';
-import image from '../assets/Home-Hero-Image.jpg';
+import { HashLink } from 'react-router-hash-link';
+// import image from '../assets/Home-Hero-Image.jpg';
 import decoration from '../assets/Decoration.svg';
 
 export default function HomeHeader(){
+
     const navigation = [
         {
             name: 'home',
-            link: '/',
+            link: '/#',
             content: 'Start'
         },
         {
             name: 'whats_going_on',
-            link: '/whats_going_on',
+            link: '/#whats_going_on',
             content: 'O co chodzi?'
         },
         {
             name: 'about_us',
-            link: '/about_us',
+            link: '/#about_us',
             content: 'O nas'
         },
         {
             name: 'fundations',
-            link: '/fundations',
+            link: '/#fundations',
             content: 'Fundacja i organizacje'
         },
         {
             name: 'contact',
-            link: '/contact',
+            link: '/#contact',
             content: 'Kontakt'
         },
     ]
 
     return (
-        <div className='grid-container'>
-            <div className='row'>
+        <div className='grid-container header'>
+            <div className='row header'>
+                <div className='col-7 header-image'/>
                 <div className='col-5'>
-                    <img src={image} alt='stare rzeczy w kartonie'/>
-                </div>
-                <div className='col-6'>
                     <section>
                         <Link to={'/logowanie'}>Zaloguj</Link>
                         <Link to={'/rejestracja'}>Załóż konto</Link>
                     </section>
                     <section>
                         <nav>
-                            <input class="menu-btn" type="checkbox" id="menu-btn" />
-                            <label class= "menu-icon" for="menu-btn"><span class="navicon"></span></label>
-                            <ul class="menu">
+                            <ul>
                             {navigation.map(({ name, link, content }) => (
                                 <li key={name}>
-                                    <Scroll to={link} spy={true} smooth={true} duration={800}>{content}</Scroll> 
+                                    <HashLink to={link} smooth>{content}</HashLink> 
                                 </li>
                             ))}
                             </ul>              
