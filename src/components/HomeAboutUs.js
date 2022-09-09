cd5b9bd342f28c06e2d3973685fd0755167d96e0
Patrycja Zalas <minekosakura@gmail.com>
2022-09-09 18:12:37 +0200

    section about us finished

--- src/components/HomeAboutUs.js
+++ src/components/HomeAboutUs.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import DecorationLine from '../utils/DecorationLine';
+import signature from '../assets/Signature.svg';
  
 export default function HomeAboutUs(){
     return (
-        <div>
-            <div>
-                <span></span>
-                
+        <section id='about_us' className='grid-container'>
+            <div className='row about-us'>
+                <div className='col-6 about-us_column'>
+                    <span className='about-us_title'>O nas</span>
+                    <DecorationLine />
+                    <span className='about-us_description'>Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. <br />Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</span>
+                    <img src={signature} className="about-us_signature" alt='podpis prezesa'/>
+                </div>
+                <div className='col-6 about-us_image'/>
             </div>
-        </div>
+        </section>
     )
 }
