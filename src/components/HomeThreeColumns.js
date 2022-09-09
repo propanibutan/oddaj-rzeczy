5a91d26fcc5806b1c9d436bd92576801fe6c79f0
Patrycja Zalas <minekosakura@gmail.com>
2022-09-09 13:27:01 +0200

    three columns section done

--- src/components/HomeThreeColumns.js
+++ src/components/HomeThreeColumns.js
@@ -1,5 +1,25 @@
 import React from 'react';
  
 export default function HomeThreeColumns(){
-    return 
+    return (
+        <section className='three-columns'>
+            <ul className='three-columns_all'>
+                <li className='one-column'>
+                    <h2 className='one-column_number'>10</h2>
+                    <h3 className='one-column_title'>ODDANYCH WORKÓW</h3>
+                    <p className='one-column_description'>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p> 
+                </li>
+                <li className='one-column'>
+                    <h2 className='one-column_number'>5</h2>
+                    <h3 className='one-column_title'>WSPARTYCH ORGANIZACJI</h3>
+                    <p className='one-column_description'>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p> 
+                </li>
+                <li className='one-column'>
+                    <h2 className='one-column_number'>5</h2>
+                    <h3 className='one-column_title'>ZORGANIZOWANY ZBIÓREK</h3>
+                    <p className='one-column_description'>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p> 
+                </li>
+            </ul>
+        </section>
+    );
 }
