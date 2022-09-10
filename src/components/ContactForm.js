8c25bd54a5f28ebeef5817b370b62b37da044be5
Patrycja Zalas <minekosakura@gmail.com>
2022-09-10 22:27:04 +0200

    successful post 200

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -20,11 +20,9 @@ export default function ContactForm({ contact, submitLabel, onSubmit }) {
         if (errorMessages) return;
 
         if (typeof onSubmit !== 'function') return;
-        onSubmit(contact(values));
+        onSubmit(values);
         setValues(contact);
     }
-
-
 
   return (
     <div className='contact-form_container'>
@@ -36,38 +34,42 @@ export default function ContactForm({ contact, submitLabel, onSubmit }) {
                 <label className='contact-form_field small'>
                     Wpisz swoje imię
                     <input 
-                        className="contact-form_field__input"
+                        id="name"
+                        className={`contact-form_field__input ${errorMessages?.name ? "error-border" : ""}`}
                         name="name"
                         type="text" 
                         placeholder="Krzysztof"
                         value={values.name}
                         onChange={handleChange}
                     />
-                    {errorMessages?.name && <span className="error-message">{' '}({errorMessages?.name})</span>}
+                    {errorMessages?.name && <span className="error-message">{' '}{errorMessages?.name}</span>}
+                    
                 </label>
                 <label className='contact-form_field small'>
                     Wpisz swój email
-                    <input 
-                        className="contact-form_field__input"
+                    <input
+                        id="email"
+                        className={`contact-form_field__input ${errorMessages?.email ? "error-border" : ""}`}
                         name="email"
                         type="text"
                         placeholder="abc@xyz.pl"
                         value={values.email}
                         onChange={handleChange}
                     />
-                    {errorMessages?.email && <span className="error-message">{' '}({errorMessages?.email})</span>}
+                    {errorMessages?.email && <span className="error-message">{' '}{errorMessages?.email}</span>}
                 </label>
             </div>
             <label className='contact-form_field large'>
                 Wpisz swoją wiadomość
-                <textarea 
-                    className="contact-form_field__input"
+                <textarea
+                    id="message"
+                    className={`contact-form_field__input ${errorMessages?.message ? "error-border" : ""}`}
                     name="message" 
                     placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
                     value={values.message}
                     onChange={handleChange}
                 />
-                {errorMessages?.message && <span className="error-message">{' '}({errorMessages?.message})</span>}
+                {errorMessages?.message && <span className="error-message">{' '}{errorMessages?.message}</span>}
             </label>
             <input className='contact-form_button' type="submit" value={submitLabel}/>
         </form>
--- src/components/contactFormValidation.js
+++ src/components/contactFormValidation.js
@@ -1,5 +1,55 @@
-function contactFormValidation() {
-    return ( <div></div> );
+//Does field is blank? If is blank return true.
+function isBlank(text) {
+    return text.trim().length === 0;
+  }
+
+//Does name is one word? If is more than one word, return true.
+function isOneWord(text) {
+    return text.trim().split(" ").length > 1;
 }
 
-export default contactFormValidation;
+//Does field is email? Return true if is correct.
+function isEmail(text) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    return regex.test(text);
+}
+
+//Does message has min. 120 letters? Return true when min 120 letters.
+function hasEnoughLetters(text) {
+    const regex2 = /[0-9a-zA-Z]{120,}/;
+
+    return regex2.test(text);
+}
+
+export default function contactFormValidation(values) {
+    const errorMessages = {};
+
+    if (isBlank(values.name)) {
+        errorMessages.name = 'Podane imię jest nieprawidłowe!';
+    }
+
+    if (isOneWord(values.name)) {
+        errorMessages.name = 'Podane imię jest nieprawidłowe!';
+    }
+
+    if (isBlank(values.email)) {
+        errorMessages.email = 'Podany email jest nieprawidłowy!';
+    }
+
+    if (isEmail(values.email) === false) {
+        errorMessages.email = 'Podany email jest nieprawidłowy!';
+    }
+
+    if (isBlank(values.message)) {
+        errorMessages.message = 'Wiadomość musi mieć conajmniej 120 znaków!';
+    }
+
+    if (hasEnoughLetters(values.message) === false) {
+        errorMessages.message = 'Wiadomość musi mieć conajmniej 120 znaków!';
+    }
+
+    return Object.keys(errorMessages).length > 0
+    ? errorMessages
+    : null;
+}
