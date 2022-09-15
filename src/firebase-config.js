b2aa6c95cb3055c2d4a8dc05385a9ee6f02de123
Patrycja Zalas <minekosakura@gmail.com>
2022-09-15 21:23:15 +0200

    tryinf sth with secure key

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: 'AIzaSyC_GcSBDRP9D2YG4bqRSdgn3OGetqjAblw',
+  authDomain: 'oddaj-rzeczy-1cdfd.firebaseapp.com',
+  projectId: 'oddaj-rzeczy-1cdfd',
+  storageBucket: 'oddaj-rzeczy-1cdfd.appspot.com',
+  messagingSenderId: '874675703558',
+  appId: '78be09f2692ff354edd887',
+  measurementId: 'G-JG6ZB8MSSW'
 };
 
 const app = initializeApp(firebaseConfig);
